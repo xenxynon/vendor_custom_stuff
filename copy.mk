cd992863bac1916bca4f893d163cbb0def976a75
xenxynon <flynryder427@gmail.com>
2024-08-24 14:30:32 +0530

    add

---
+++ copy.mk
@@ -0,0 +1,7 @@
+PRODUCT_SOONG_NAMESPACES += vendor/custom/stuff
+
+PRODUCT_COPY_FILES += \
+ChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/ChargingStarted.ogg \
+Lock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Lock.ogg
+Unlock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Unlock.ogg
+LowBattery.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/LowBattery.ogg \
