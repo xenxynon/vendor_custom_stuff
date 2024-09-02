37f195a1cb571b7e3097a2f96c7fd9ab9b60f58c
xenxynon <flynryder427@gmail.com>
2024-09-02 10:49:55 +0530

    Update copy.mk

--- copy.mk
+++ copy.mk
@@ -1,8 +1,15 @@
 PRODUCT_SOONG_NAMESPACES += vendor/custom/stuff
-PC_PATH=$(LOCAL_PATH)
-PRODUCT_COPY_FILES += \
 
-$(PC_PATH)/ChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/ChargingStarted.ogg \
-$(PC_PATH)/Lock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Lock.ogg \
-$(PC_PATH)/Unlock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Unlock.ogg \
-$(PC_PATH)/LowBattery.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/LowBattery.ogg
+PC_PATH=vendor/custom/stuff
+
+ifeq (Spacewar ,$(TARGET_PRODUCT))
+
+PRODUCT_COPY_FILES += \
+      $(PC_PATH)/ChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/ChargingStarted.ogg \
+      $(PC_PATH)/Lock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Lock.ogg \
+      $(PC_PATH)/Unlock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Unlock.ogg \
+      $(PC_PATH)/LowBattery.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/LowBattery.ogg
+
+DEVICE_PACKAGE_OVERLAYS += \
+       $(PC_PATH)/overlay
+endif
