228642dc47e9a3449f1da557dab8ea66ba207163
xenxynon <flynryder427@gmail.com>
2024-09-17 14:50:51 +0000

    organize

--- copy.mk
+++ copy.mk
@@ -5,11 +5,11 @@ PC_PATH=vendor/custom/stuff
 ifeq (Spacewar ,$(TARGET_PRODUCT))
 
 PRODUCT_COPY_FILES += \
-      $(PC_PATH)/ChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/ChargingStarted.ogg \
-      $(PC_PATH)/Lock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Lock.ogg \
-      $(PC_PATH)/Unlock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Unlock.ogg \
-      $(PC_PATH)/LowBattery.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/LowBattery.ogg \
-      $(PC_PATH)/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+      $(PC_PATH)/product/media/audio/ui/ChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/ChargingStarted.ogg \
+      $(PC_PATH)/product/media/audio/ui/Lock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Lock.ogg \
+      $(PC_PATH)/product/media/audio/ui/Unlock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Unlock.ogg \
+      $(PC_PATH)/product/media/audio/ui/LowBattery.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/LowBattery.ogg \
+      $(PC_PATH)/product/media/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 
 DEVICE_PACKAGE_OVERLAYS += \
        $(PC_PATH)/overlay
